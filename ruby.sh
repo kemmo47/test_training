bb566f0fdfa123d7a7e426d7c5aa4741169f125d
kemmo47 <longkem123456@gmail.com>
2022-10-08 21:51:11 +0700

    add bootstrap

--- ruby.sh
+++ ruby.sh
@@ -2,27 +2,32 @@
 
 echo "================================================================================"
 
-if [ ! -d "/my_ruby/node_modules" ]
-then
-    echo "node_modules dose not exist!"
-    cd my_ruby || exit
-    npm install
-else
-    echo "node_modules dose exist!"
-    if [ ! -d "/my_ruby/node_modules/sweetalert2" ]
-    then
-        echo "sweetalert2 dose not exist!"
-        cd my_ruby || exit
-        rm -rf node_modules && rm -rf package-lock.json yarn.lock && npm install
-    else
-        echo "sweetalert2 dose exist!"
-        cd my_ruby || exit
-    fi
-fi
+# if [ ! -d "/my_ruby/node_modules" ]
+# then
+#     echo "node_modules dose not exist!"
+#     cd my_ruby || exit
+#     npm install
+# else
+#     echo "node_modules dose exist!"
+#     if [ ! -d "/my_ruby/node_modules/sweetalert2" ]
+#     then
+#         echo "sweetalert2 dose not exist!"
+#         cd my_ruby || exit
+#         rm -rf node_modules && rm -rf package-lock.json yarn.lock && npm install
+#     else
+#         echo "sweetalert2 dose exist!"
+#         cd my_ruby || exit
+#     fi
+# fi
+# echo "--------------------- bundle install ---------------------"
+# bundle install
 
-bundle install
-rails db:migrate RAILS_ENV=development
-rails webpacker:install
+# echo "--------------------- rails db:migrate RAILS_ENV=development ---------------------"
+# rails db:migrate RAILS_ENV=development
+
+echo "--------------------- rm -f tmp/pids/server.pid ---------------------"
 rm -f tmp/pids/server.pid
+
+echo "--------------------- rails server -b 0.0.0.0 ---------------------"
 rails server -b 0.0.0.0
 exec "$@"
